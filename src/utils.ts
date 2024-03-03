152bda53fc9e289fce9c4f1f80a145d020ac927e
Reda Bacha <redabacha01@gmail.com>
2024-03-03 21:47:38 +0000

    refactor: remove extraneous changes to utils

--- src/utils.ts
+++ src/utils.ts
@@ -101,12 +101,11 @@ export async function findProjectDir(
     return result;
   }
 
-  // this could either be yarn v1 or yarn berry (v2+)
   const yarnLockFile = path.join(dir, "yarn.lock");
   if (await fileExists(yarnLockFile)) {
     logDebug(`Detected yarn from lockfile ${yarnLockFile}`);
-    result.pkgManagerName = "yarn";
     result.projectDir = dir;
+    result.pkgManagerName = "yarn";
     return result;
   }
 
